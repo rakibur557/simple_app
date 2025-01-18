ed6721939260b8ffb3d792ca9e8f193b2ef5e704
rakibur557 <rakiburrahman.cse.eng@gmail.com>
2025-01-18 12:44:57 +0600

    class 8

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,44 +1,157 @@
 import 'package:flutter/material.dart';
-import 'package:simple_app/profile.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.red,
-        title: Text(
-          'Jamuna Tv',
-          style: TextStyle(
-            color: Colors.white,
-          ),
-        ),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.red,
-            elevation: 10,
-            shadowColor: Colors.red,
-            side: BorderSide(
-              color: Colors.green,
-              width: 1,
+      body: Padding(
+        padding: const EdgeInsets.only(top: 14, left: 14, right: 14),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          spacing: 20,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Hi there,',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    Text(
+                      'Rakibur Rahman',
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                CircleAvatar(
+                  radius: 25,
+                  backgroundImage: NetworkImage(
+                      'https://media.istockphoto.com/id/1406197730/photo/portrait-of-a-young-handsome-indian-man.jpg?s=612x612&w=0&k=20&c=CncNUTbw6mzGsbojks2Vt0kV85N_pQaI3zaSkBQJFTc='),
+                ),
+              ],
             ),
-          ),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const ProfileScreen()),
-            );
-          },
-          child: Text(
-            'Go to profile screen',
-            style: TextStyle(
-              color: Colors.white,
+            TextFormField(
+              decoration: InputDecoration(
+                hintText: "Search",
+                hintStyle: TextStyle(color: Colors.grey),
+                suffixIcon: Icon(Icons.search_rounded),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(color: Colors.grey),
+                ),
+              ),
             ),
-          ),
+            Text(
+              'Course Catagories',
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  spacing: 10,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Color(0xffa093d8),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(14),
+                        child: Icon(
+                          Icons.ac_unit_outlined,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    Text('UX')
+                  ],
+                ),
+                Column(
+                  spacing: 10,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Color(0xfffbbe30),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(14),
+                        child: Icon(
+                          Icons.account_balance_wallet_outlined,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    Text('HCI')
+                  ],
+                ),
+                Column(
+                  spacing: 10,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Color(0xff4fd7d4),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(14),
+                        child: Icon(
+                          Icons.rocket_launch_rounded,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    Text('Design')
+                  ],
+                ),
+                Column(
+                  spacing: 10,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Color(0xfffe7892),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(14),
+                        child: Icon(
+                          Icons.ac_unit_outlined,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    Text('Motion')
+                  ],
+                ),
+              ],
+            ),
+            Text(
+              'Enrolled Courses',
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                border: Border.all(color: Colors.grey),
+              ),
+              child: Column(
+                children: [],
+              ),
+            ),
+          ],
         ),
       ),
     );
